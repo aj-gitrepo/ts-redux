d44b7cc97bc8aee15768d9c1ffae7fea561eba35
wink-aajlak <aajlak.wink@gmail.com>
2022-02-16 13:24:43 +0530

    first commit

---
+++ notes.tsx
@@ -0,0 +1,11 @@
+// >npx create-react-app ts-redux --template typescript
+// npm install --save-exact @types/react-redux@7.1.15 axios@0.21.1 react-redux@7.2.2 redux@4.0.5 redux-thunk@2.3.0 @types/react
+
+// >yarn add @types/react-redux@7.1.15 axios@0.21.1 react-redux@7.2.2 redux@4.0.5 redux-thunk@2.3.0 @types/react -exact
+  // >yarn add axios
+  // >yarn add redux react-redux redux-thunk
+  // >yarn add @types/react
+  // >yarn add @types/react-redux
+
+// querying the npm registry api
+// registry.npmjs.org/-/v1/search?text=react
